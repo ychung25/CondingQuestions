6c0af907a58ceb5cba1e3bde40a4595e7c866890
Yongik Chung <yochung@microsoft.com>
2015-11-04 12:16:59 -0800

    sdfsdfsdf

--- Coding/Coding/CircularLinkedList.h
+++ Coding/Coding/CircularLinkedList.h
@@ -14,242 +14,118 @@ class CLL
 public:
 	CLL() : m_head(nullptr) {}
 
-	void InsertAfterHead(CLLNode* node) 
-	{
-		if (m_head == nullptr)
-		{
-			m_head = node;
-			m_head->next = node;
-		}
-		else if (m_head->next == m_head)
-		{
-			m_head->next = node;
-			node->next = m_head;
-		}
-		else if (m_head != m_head)
-		{
-			CLLNode* afterHead = m_head->next;
-			m_head->next = node;
-			node->next = afterHead;
-		}
-	}
-	void InsertBeforeHead(CLLNode* node) 
-	{
-		if (m_head == nullptr)
-		{
-			m_head = node;
-			m_head->next = node;
-		}
-		else if (m_head->next == m_head)
-		{
-			m_head->next = node;
-			node->next = m_head;
-		}
-		else if(m_head != m_head)
-		{
-			CLLNode* beforeHead = m_head->next;
-			while (beforeHead->next == m_head)
-			{
-				beforeHead = beforeHead->next;
-			}
-			beforeHead->next = node;
-			node->next = m_head;
-
-		}
-	}
 	void InsertNewHead(CLLNode* node)
 	{
 		if (m_head == nullptr)
 		{
 			m_head = node;
-			m_head->next = node;
+			m_head->next = m_head;
 		}
-		else if (m_head->next == m_head)
+		else
 		{
-			m_head->next = node;
-			node->next = m_head;
+			InsertBeforeTarget(m_head, node);
 			m_head = node;
-		}
-		else if (m_head != m_head)
-		{
-			CLLNode* beforeHead = m_head->next;
-			while (beforeHead->next == m_head)
-			{
-				beforeHead = beforeHead->next;
-			}
-			beforeHead->next = node;
-			node->next = m_head;
-			m_head = node;
-
 		}
 	}
 	void InsertAfterTarget(CLLNode* target, CLLNode* node) 
 	{
 		if (target->next == target)
 		{
-			m_head->next = node;
-			node->next = m_head;
+			target->next = node;
+			node->next = target;
 		}
 		else
 		{
-			CLLNode* afterTarget = target->next;
-			target->next = node;
-			node->next = afterTarget;
+			CLLNode* current = target;
+			CLLNode* nextCurrent = target->next;
+			current->next = node;
+			node->next = nextCurrent;
 		}
 	}
 	void InsertBeforeTarget(CLLNode* target, CLLNode* node) 
 	{
 		if (target->next == target)
 		{
-			m_head->next = node;
-			node->next = m_head;
-		}
-		{
-			CLLNode* beforeTarget = m_head;
-			while (beforeTarget->next != target)
-			{
-				beforeTarget = beforeTarget->next;
-			}
-
-			beforeTarget->next = node;
+			target->next = node;
 			node->next = target;
 		}
+		else
+		{
+			CLLNode* current = target;
+			CLLNode* nextCurrent = target->next;
+			while (nextCurrent->next != target)
+			{
+				nextCurrent = nextCurrent->next;
+			}
+			node->next = target;
+			nextCurrent->next = node;
+		}
 	}
 
-	CLLNode* RemoveAfterHead()
-	{
-		if (m_head == nullptr) { return nullptr; }
-		if (m_head->next == m_head)
-		{
-			CLLNode* temp = m_head;
-			m_head = nullptr;
-			temp->next = nullptr;
-			return temp;
-		}
-		else
-		{
-			CLLNode* afterHead = m_head->next;
-			CLLNode* afterAfterHead = afterHead->next;
-			if (afterAfterHead == m_head)
-			{
-				m_head->next = m_head;
-				afterHead->next = nullptr;
-				return afterHead;
-			}
-			else
-			{
-				m_head->next = afterAfterHead;
-				afterHead->next = nullptr;
-				return afterHead;
-			}
-		}
-	}
-	CLLNode* RemoveBeforeHead()
-	{
-		if (m_head->next == m_head)
-		{
-			CLLNode* temp = m_head;
-			m_head = nullptr;
-			temp->next = nullptr;
-			return temp;
-		}
-		else
-		{
-			CLLNode* afterHead = m_head;
-			CLLNode* afterAfterHead = afterHead->next;
-			if (afterAfterHead == m_head)
-			{
-				m_head->next = m_head;
-				afterHead->next = nullptr;
-				return afterHead;
-			}
-			else
-			{
-				while (afterAfterHead->next != m_head)
-				{
-					afterHead = afterHead->next;
-					afterAfterHead = afterAfterHead->next;
-				}
-				afterHead->next = m_head;
-				afterAfterHead->next = nullptr;
-				return afterAfterHead;
-			}
-		}
-	}
 	CLLNode* RemoveAfterTarget(CLLNode* target)
 	{
-		if (m_head->next == m_head)
+		if (target->next == target)
 		{
-			CLLNode* temp = m_head;
+			target->next = nullptr;
 			m_head = nullptr;
-			temp->next = nullptr;
-			return temp;
 		}
 		else
 		{
-			CLLNode* afterTarget = target;
-			CLLNode* afterAfterTarget = afterTarget->next;
-			if (afterAfterTarget == target)
-			{
-				m_head->next = m_head;
-				afterTarget->next = nullptr;
-				return afterTarget;
-			}
-			else
-			{
-				target->next = afterAfterTarget;
-				afterTarget->next = nullptr;
-				return afterTarget;
+			CLLNode* current = target;
+			CLLNode* nextCurrent = target->next;
+			CLLNode* nextNextCurrent = nextCurrent->next;
+	
+			target->next = nextNextCurrent;
+			nextCurrent->next = nullptr;
 
+			if (nextCurrent == m_head)
+			{
+				m_head = target;
 			}
+
+			return nextCurrent;
 		}
 	}
 	CLLNode* RemoveBeforeTarget(CLLNode* target)
 	{
-		if (m_head->next == m_head)
+		if (target->next == target)
 		{
-			CLLNode* temp = m_head;
+			target->next = nullptr;
 			m_head = nullptr;
-			temp->next = nullptr;
-			return temp;
 		}
 		else
 		{
-			CLLNode* afterTarget = target;
-			CLLNode* afterAfterTarget = afterTarget->next;
-			if (afterAfterTarget == target)
+			CLLNode* current = target;
+			CLLNode* nextCurrent = target->next;
+			CLLNode* nextNextCurrent = nextCurrent->next;
+			while (nextNextCurrent->next != target)
 			{
-				m_head->next = m_head;
-				afterTarget->next = nullptr;
-				return afterTarget;
+				nextCurrent = nextCurrent->next;
+				nextNextCurrent = nextNextCurrent->next;
 			}
-			else
-			{
-				while (afterAfterTarget->next != target)
-				{
-					afterTarget = afterTarget->next;
-					afterAfterTarget = afterAfterTarget->next;
-				}
-				afterTarget->next = target;
-				afterAfterTarget->next = nullptr;
-				return afterAfterTarget;
 
+			nextCurrent->next = target;
+			nextNextCurrent->next = nullptr;
+			if (nextNextCurrent == m_head)
+			{
+				m_head = nextCurrent;
 			}
+			return nextNextCurrent;
 		}
 	}
 
 	void Traverse()
 	{
-		printf("CLL content");
-		CLLNode* temp = m_head;
-		while (temp->next != m_head)
+		printf("DLL content");
+		if (m_head == nullptr) { return; }
+		CLLNode* current = m_head;
+		do
 		{
-			printf("%d ", temp->data);
-			temp = temp->next;
-		}
+			printf("%d ", current->data);
+			current = current->next;
+		} while (current != m_head);
 		printf("\n");
 	}
-
 
 private:
 	CLLNode* m_head;
@@ -266,11 +142,16 @@ void DoCircularLinkedList()
 	CLLNode node7(7);
 
 	CLL cll;
-	cll.InsertAfterHead(&node4);
-	cll.InsertAfterHead(&node6);
-	cll.InsertBeforeTarget(&node6, &node5);
-	cll.InsertBeforeHead(&node7);
+	cll.InsertNewHead(&node3);
+	cll.InsertNewHead(&node1);
+	cll.InsertAfterTarget(&node1, &node2);
+	cll.InsertAfterTarget(&node3, &node5);
+	cll.InsertBeforeTarget(&node5, &node4);
+	cll.InsertAfterTarget(&node5, &node6);
+	cll.InsertAfterTarget(&node6, &node7);
 	cll.Traverse();
 
-
+	cll.RemoveAfterTarget(&node1);
+	cll.RemoveBeforeTarget(&node1);
+	cll.Traverse();
 }
