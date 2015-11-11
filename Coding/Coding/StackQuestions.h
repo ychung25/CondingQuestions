6a53cfceb46e4f5c7ae2b8932db86ee5f3c1ef6e
yongik chung <ychung25@hotmail.com>
2015-11-10 17:41:34 -0800

    fd

--- Coding/Coding/StackQuestions.h
+++ Coding/Coding/StackQuestions.h
@@ -4,6 +4,17 @@
 
 namespace StackQuestions
 {
+	int stringLength(char* str)
+	{
+		int i = 0;
+		while (*str)
+		{
+			i++;
+			str++;
+		}
+		return i;
+	}
+
 	// An expression can be wrapped with { }, ( ) or [ ].
 	// E.g. (A[0]+B[0]){}    <- valid
 	//      (A[0] + B[0])){} <- invalid
@@ -51,7 +62,7 @@ namespace StackQuestions
 		return true;
 	}
 
-	void GetMininumStackV1()
+	void GetMininumStack()
 	{
 		class GetMinStack
 		{
@@ -192,6 +203,243 @@ namespace StackQuestions
 
 	}
 
+	// You can also solve it with by converting it to LL. Space O(c), Time (n)
+	// This implementation uses stack so Space O(n), Time O(n)
+	bool IsItPalindrome(char* string, int len)
+	{
+		int mid = len / 2;
+
+		Stack stack;
+		for (int i = 0; i < mid; i++)
+		{
+			stack.push(string);
+			string++;
+		}
+
+		if (len % 2)
+		{
+			string++;
+		}
+
+		while (stack.Size() > 0)
+		{
+			char* x = (char*)stack.pop();
+			if (*x != *string)
+				return false;
+			string++;
+		}
+
+		if (*string)
+			return false;
+
+		return true;
+	}
+	 
+	// This is FUCKING AWESOME!!!!! Use only on stack to reverse stack.
+	void FlipIt(Stack* s, void* toInsert)
+	{
+		if (s->Size() == 0)
+		{
+			s->push(toInsert);
+		}
+		else
+		{
+			void* data = s->pop();
+			FlipIt(s, toInsert);
+			s->push(data);
+		}
+	}
+	void ReverseIt(Stack* s)
+	{
+		if (s->Size() == 0)
+			return;
+		
+		void* data = s->pop();
+		ReverseIt(s);
+		FlipIt(s, data);
+	}
+
+	// You can also do the same to build a stack using two queues
+	void QueueUsingTwoStacks()
+	{
+		class Queue
+		{
+		public:
+			void push(void* data)
+			{
+				if (s2.Size() > 0)
+				{
+					s1.push(s2.pop());
+				}
+				s1.push(data);
+
+			}
+			void* pop()
+			{
+				void* data = 0;
+				while (s1.Size() > 0)
+				{
+					s2.push(s1.pop());
+				}
+				
+				data = s2.pop();
+				return data;
+
+			}
+		private:
+			Stack s1;
+			Stack s2;
+		};
+
+		int i1 = 9;
+		int i2 = 3;
+		int i3 = 4;
+		Queue queue;
+		queue.push(&i1);
+		queue.push(&i2);
+		queue.push(&i3);
+
+		int* data = (int*)queue.pop(); //9
+		data = (int*)queue.pop(); //3
+
+		queue.push(&i3);
+		queue.push(&i2);
+	    data = (int*)queue.pop(); //4
+		data = (int*)queue.pop(); //4
+	}
+
+	// p - push
+	// x - pop
+	// pxpx  valid
+	// pxx   invalid
+	// pppxx valid
+	bool IsValidPushPopOpeartions(char* pushesAndPops, int len)
+	{
+		int pushCount = 0;
+		int popCount = 0;
+
+		for (int i = 0; i < len; i++)
+		{
+			if (*pushesAndPops == 'p')
+				pushCount++;
+			if (*pushesAndPops == 'x')
+			{
+				popCount++;
+				if (popCount > pushCount)
+				{
+					return false;
+				}
+			}
+
+			pushesAndPops++;
+		}
+
+		return true;
+	}
+
+	void FindLargestRectInHistogram()
+	{
+		int histogram[] = { 1,2,2,6,5,1 };
+		int len = sizeof(histogram) / sizeof(histogram[0]);
+
+		int max = 0;
+		int rangeStart = 0;
+		int rangeEnd = 0;
+		int maxRangeStart = 0;
+		int maxRangeEnd = 0;
+
+		for (int i = 0; i < len; i++)
+		{
+			int begin = i;
+			int end = i;
+
+			int curentValue = histogram[i];
+			int rangeStart = i;
+			int rangeEnd = i;
+
+			while (begin >= 0 || end < len)
+			{
+				begin = begin - 1; 
+				end = end + 1;
+
+				if (begin >= 0)
+				{
+					if (histogram[begin] >= histogram[i])
+					{
+						curentValue += histogram[i];
+						rangeStart = begin;
+					}
+					else
+					{
+						begin = -1;
+					}
+				}
+
+				if (end < len)
+				{
+					if (histogram[end] >= histogram[i])
+					{
+						curentValue += histogram[i];
+						rangeEnd = end;
+					}
+					else
+					{
+						end = len;
+					}
+				}
+			}
+			if (curentValue > max)
+			{
+				max = curentValue;
+				maxRangeStart = rangeStart;
+				maxRangeEnd = rangeEnd;
+			}
+		}
+	}
+
+	void RemoveAdjacentDuplicates(char* string, int len)
+	{
+		Stack stack;
+		for (int i = 0; i < len; i++)
+		{
+			char c = *string;
+			void* data = stack.peek();
+			if (data)
+			{
+				char cStack = *(char*)data;
+				if (c == cStack)
+				{
+					stack.pop();
+				}
+				else
+				{
+					stack.push(string);
+				}
+
+			}
+			else
+			{
+				stack.push(string);
+			}
+
+			string++;
+		}
+
+		printf("\nRemoveAdjacentDuplicates-----\n");
+		Stack temp;
+		while (stack.Size() > 0)
+		{
+			temp.push(stack.pop());
+		}
+		while (temp.Size() > 0)
+		{
+			printf("%c", *((char*)temp.pop()));
+		}
+		printf("\n\n");
+	}
+
+
+
 	void DoStackQuestions()
 	{
 		{
@@ -203,7 +451,45 @@ namespace StackQuestions
 		}
 
 		{
-			GetMininumStackV1();
+			GetMininumStack();
+		}
+
+		{
+			bool result = IsItPalindrome("abccbaa", 6);
+		}
+
+		{
+			Stack stack;
+			char c1 = 'c';
+			char c2 = 'b';
+			char c3 = 'a';
+			stack.push(&c1);
+			stack.push(&c2);
+			stack.push(&c3);
+
+			ReverseIt(&stack);
+
+			char* c = (char*)stack.pop();
+			char* b = (char*)stack.pop();
+			char* a = (char*)stack.pop();
+		}
+
+		{
+			QueueUsingTwoStacks();
+		}
+
+		{
+			char* string = "pxpxpxppxxppxpxxppx";
+			bool result = IsValidPushPopOpeartions(string, stringLength(string));
+		}
+
+		{
+			FindLargestRectInHistogram();
+		}
+
+		{
+			char* str = "mississippi";
+			RemoveAdjacentDuplicates(str, stringLength(str));
 		}
 	}
 }
