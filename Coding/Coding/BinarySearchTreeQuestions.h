f9ca15a47bb851ab99024f43e1136324385458d9
yongik chung <ychung25@hotmail.com>
2015-11-15 00:36:12 -0800

    sdfdf

--- Coding/Coding/BinarySearchTreeQuestions.h
+++ Coding/Coding/BinarySearchTreeQuestions.h
@@ -132,6 +132,61 @@ namespace BinarySearchTreeQuestions
 			findLCD(k->r, a, b);
 	}
 
+	// Do inorder and if prev > current, it is not BST
+	bool isBST(Node* n, int p)
+	{
+		if (!n)
+			return true;
+
+		bool bLeft = isBST(n->l, n->data);
+
+		if (p > n->data)
+			return false;
+		p = n->data;
+
+		bool bRight = isBST(n->r, n->data);
+
+		return bLeft && bRight;
+	}
+
+	Node* BST2DLL(Node* n)
+	{
+		if (!n)
+			return 0;
+
+		Node* leftTail = BST2DLL(n->l);
+		Node* rightTail = BST2DLL(n->r);
+
+		if (!leftTail && !rightTail)
+		{
+			return n;
+		}
+		if (leftTail && !rightTail)
+		{
+			Node* l = n->l;
+			l->r = n;
+			rightTail = leftTail;
+		}
+		else if (!leftTail && rightTail)
+		{
+			Node* r = n->r;
+			n->l = r;
+			r->r = n;
+		}
+		else if (leftTail && rightTail)
+		{
+			Node* r = n->r;
+			leftTail->l = r;
+			leftTail->r = n;
+			r->r = leftTail;
+		}
+
+		n->r = rightTail;
+		rightTail->l = n;
+
+		return rightTail;
+	}
+
 	void DoBinarySearchTreeQuestions()
 	{
 		Node* n2 = createNode(2, 0, 0);
@@ -156,6 +211,25 @@ namespace BinarySearchTreeQuestions
 
 		int ilcd = findLCD(n20, 21, 40);
 
+		bool bBST = isBST(n20, n20->data);
+	
+
+		{
+			Node* n2 = createNode(2, 0, 0);
+			Node* n5 = createNode(5, n2, 0);
+			Node* n17 = createNode(17, 0, 0);
+			Node* n16 = createNode(16, 0, n17);
+			Node* n15 = createNode(15, n5, n16);
+			Node* n40 = createNode(40, 0, 0);
+			Node* n35 = createNode(35, 0, n40);
+			Node* n25 = createNode(25, 0, 0);
+			Node* n30 = createNode(30, n25, n35);
+			Node* n20 = createNode(20, n15, n30);
+
+			Node* dll = BST2DLL(n20);
+			printf("");
+		}
+		
 		printf("");
 
 	}
