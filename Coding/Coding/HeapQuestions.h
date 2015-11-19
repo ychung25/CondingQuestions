bab29b936aa3af8203320f184c64de571a7f67db
Yongik Chung <yochung@microsoft.com>
2015-11-18 18:04:44 -0800

    dsfsd

--- Coding/Coding/HeapQuestions.h
+++ Coding/Coding/HeapQuestions.h
@@ -1,8 +1,83 @@
 #pragma once
 #include "Heap.h"
 
+/* ***********************************************************
+  All Heaps here are MinHeap for the sake of question solving
+**************************************************************/
 namespace HeapQuestions
 {
+	void HeapifyUp(int index, int data[])
+	{
+		if (index == 0)
+			return;
+
+		int parentIndex = (index - 1) / 2;
+		if (parentIndex < 0)
+			parentIndex = 0;
+
+		if (data[parentIndex] > data[index])
+		{
+			int temp = data[index];
+			data[index] = data[parentIndex];
+			data[parentIndex] = temp;
+		}
+	}
+	void HeapifyDown(int index, int data[], int size)
+	{
+		int lIndex = (2 * index) + 1;
+		int rIndex = (2 * index) + 2;
+
+		if (lIndex < size && rIndex < size)
+		{
+			if (data[lIndex] < data[index])
+			{
+				if (data[lIndex] < data[index])
+				{
+					int temp = data[index];
+					data[lIndex] = data[index];
+					data[lIndex] = temp;
+					HeapifyDown(lIndex, data, size);
+				}
+			}
+			else
+			{
+				if (data[rIndex] < data[index])
+				{
+					int temp = data[index];
+					data[rIndex] = data[index];
+					data[rIndex] = temp;
+					HeapifyDown(rIndex, data, size);
+				}
+			}
+		}
+		else if (lIndex < size)
+		{
+			if (data[lIndex] < data[index])
+			{
+				int temp = data[index];
+				data[lIndex] = data[index];
+				data[lIndex] = temp;
+				HeapifyDown(lIndex, data, size);
+			}
+		}
+	}
+
+	void CreateHeapFromArrayInPlace()
+	{
+		int data[] = { 5,3,2,4,1,7,6 };
+		int dataSize = 7;
+		for (int i = dataSize - 1; i >= 0; i--)
+		{
+			HeapifyUp(i, data);
+		}
+
+		for (int i = 0; i < dataSize; i++)
+		{
+			HeapifyDown(0, data, dataSize - i);
+		}
+
+
+	}
 	void HeapSortClassic()
 	{
 		int data[] = { 5,3,2,4,1,7,6 };
